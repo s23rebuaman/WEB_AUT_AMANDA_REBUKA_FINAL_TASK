d07e844fc3bd653e090d0aa9e55e3da8c23f5985
s23rebuaman <s23rebuaman@venta.lv>
2026-04-28 14:59:23 +0300

    enters and validates information

--- cypress/e2e/web-auto.cy.js
+++ cypress/e2e/web-auto.cy.js
@@ -15,7 +15,41 @@ describe("Demoqa scenario", () => {
       FormsPage.practiceForm.click();
       // Input all the necessary information in the text fields,
       // buttons, etc, where it is not stated otherwise.
-      //PracticeFormPage.welp();
+      PracticeFormPage.firstName.type('Amanda');
+      PracticeFormPage.lastName.type('Rebuka');
+      PracticeFormPage.userEmail.type('amanda@example.com');
+      PracticeFormPage.gender.click();
+      PracticeFormPage.userMobileNumber.type('1029384756');
+      PracticeFormPage.birthDate.click();
+      PracticeFormPage.birthYear.select('1930');
+      PracticeFormPage.birthMonth.select('February');
+      PracticeFormPage.birthDay.click();
+      // ! ! ! Vajadzēja DevTools console rakstīt "setTimeout(() => { debugger; }, 5000);", ! ! !
+      // ! ! ! lai paspētu atrast list objektu selectors, savādāk tie uzreiz aizvērās       ! ! !
+      PracticeFormPage.subjects.type('Economics');
+      PracticeFormPage.economicsSubject.click();
+      PracticeFormPage.hobbies.click();
+      PracticeFormPage.picture.click();
+      PracticeFormPage.selectPicture;
+      PracticeFormPage.address.type('Example address 123');
+      PracticeFormPage.state.type('NCR');
+      PracticeFormPage.stateNCR.click();
+      PracticeFormPage.city.type('Delhi');
+      PracticeFormPage.cityDelhi.click();
+      PracticeFormPage.submitButton.click();
+      // Validate that each Labeled row contains the correct information.
+      PracticeFormPage.studentName.should('contain.text','Amanda Rebuka');
+      PracticeFormPage.studentEmail.should('contain.text','amanda@example.com');
+      PracticeFormPage.studentGender.should('contain.text','Female');
+      PracticeFormPage.studentMobile.should('contain.text','1029384756');
+      // ! ! ! izvēlējos citu datums, jo kodu rakstu 28.datumā, un tāpēc tas datums jau ! ! !
+      // ! ! ! ir izvēlēts un tāpēc tas man veido problēmas kodā                        ! ! !
+      PracticeFormPage.studentBirthday.should('contain.text','18 February,1930');
+      PracticeFormPage.studentSubjects.should('contain.text','Economics');
+      PracticeFormPage.studentHobbies.should('contain.text','Music');
+      PracticeFormPage.studentPicture.should('contain.text','C:\\Users\\Dators\\Downloads\\web_automation\\WEB_AUT_AMANDA_REBUKA_FINAL_TASK\\cypress\\files\\meme cat.png');
+      PracticeFormPage.studentAddress.should('contain.text','Example address 123');
+      PracticeFormPage.studentStateAndCity.should('contain.text','NCR Delhi');
     });
   });
 });
--- cypress/pageObjects/practiceFormPage.js
+++ cypress/pageObjects/practiceFormPage.js
@@ -4,4 +4,124 @@ export class PracticeFormPage extends BasePage{
     static get url() {
         return "/#/automation-practice-form";
     }
+
+    static get firstName() {
+        return cy.get('#firstName');
+    }
+
+    static get lastName() {
+        return cy.get('#lastName');
+    }
+
+    static get userEmail() {
+        return cy.get('#userEmail');
+    }
+
+    static get gender() {
+        return cy.get('#gender-radio-2');
+    }
+
+    static get userMobileNumber() {
+        return cy.get('#userNumber');
+    }
+
+    static get birthDate() {
+        return cy.get('#dateOfBirthInput');
+    }
+
+    static get birthYear() {
+        return cy.get('[class="react-datepicker__year-select"]');
+    }
+
+    static get birthMonth() {
+        return cy.get('[class="react-datepicker__month-select"]');
+    }
+
+    static get birthDay(){
+        return cy.get('[class="react-datepicker__day react-datepicker__day--018"]');
+    }
+
+    static get subjects(){
+        return cy.get('[class="subjects-auto-complete__control css-13cymwt-control"]');
+    }
+    
+    static get economicsSubject(){
+        return cy.get('#react-select-2-listbox');
+    }
+
+    static get hobbies(){
+        return cy.get('#hobbies-checkbox-3');
+    }
+
+    static get picture(){
+        return cy.get('#uploadPicture');
+    }
+    //cy.get('input[type=file]').selectFile('file.json')
+    static get selectPicture(){
+        return cy.get('#uploadPicture').selectFile('C:\\Users\\Dators\\Downloads\\web_automation\\WEB_AUT_AMANDA_REBUKA_FINAL_TASK\\cypress\\files\\meme cat.png');
+    }
+
+    static get address(){
+        return cy.get('#currentAddress');
+    }
+
+    static get state(){
+        return cy.get('[class="css-13cymwt-control"]');
+    }
+    
+    static get stateNCR(){
+        return cy.get('#react-select-3-option-0');
+    }
+
+    static get city(){
+        return cy.get('[class="css-13cymwt-control"]');
+    }
+    
+    static get cityDelhi(){
+        return cy.get('#react-select-4-option-0');
+    }
+
+    static get submitButton(){
+        return cy.get('#submit');
+    }
+
+    static get studentName(){
+        return cy.get('[class="table table-dark table-striped table-bordered table-hover"]').find('td').eq(1);
+    }
+
+    static get studentEmail(){
+        return cy.get('[class="table table-dark table-striped table-bordered table-hover"]').find('td').eq(3);
+    }
+
+    static get studentGender(){
+        return cy.get('[class="table table-dark table-striped table-bordered table-hover"]').find('td').eq(5);
+    }
+
+    static get studentMobile(){
+        return cy.get('[class="table table-dark table-striped table-bordered table-hover"]').find('td').eq(7);
+    }
+
+    static get studentBirthday(){
+        return cy.get('[class="table table-dark table-striped table-bordered table-hover"]').find('td').eq(9);
+    }
+
+    static get studentSubjects(){
+        return cy.get('[class="table table-dark table-striped table-bordered table-hover"]').find('td').eq(11);
+    }
+
+    static get studentHobbies(){
+        return cy.get('[class="table table-dark table-striped table-bordered table-hover"]').find('td').eq(13);
+    }
+
+    static get studentPicture(){
+        return cy.get('[class="table table-dark table-striped table-bordered table-hover"]').find('td').eq(15);
+    }
+
+    static get studentAddress(){
+        return cy.get('[class="table table-dark table-striped table-bordered table-hover"]').find('td').eq(17);
+    }
+
+    static get studentStateAndCity(){
+        return cy.get('[class="table table-dark table-striped table-bordered table-hover"]').find('td').eq(19);
+    }
 }
